efe2f6d718aa221184a35c4b35cd2f6d760ab462
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2017-08-09 10:42:27 +0100

    Signed-off-by: ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>

--- 3ATechnologies/src/main/java/com/Application.java
+++ 3ATechnologies/src/main/java/com/Application.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.controller.StartUpInit;
+import com.startup.StartUpInit;
 
 @SpringBootApplication
 public class Application {
--- 3ATechnologies/src/main/java/com/startup/StartUpInit.java
+++ 3ATechnologies/src/main/java/com/startup/StartUpInit.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.startup;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
